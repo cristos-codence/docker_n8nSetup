99ffeda64319857a4386ac40435a88205f59de01
Cristos <cristos@cristoslc.com>
2025-02-14 16:47:14 -0500

    fix: Squash merge aider-cristos branch into development

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@ git checkout aider-cristos
 git add .
 aider --commit --no-restore-chat-history
 git checkout development
-git merge aider-cristos
+git merge --squash aider-cristos
 git push
 git checkout aider-cristos
